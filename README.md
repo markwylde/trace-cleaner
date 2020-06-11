4da3355f485e2dcc3224a1bbc0f3752a2bf736e4
Mark Wylde <me@markwylde.com>
2020-06-11 22:54:37 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# trace-cleaner
+Clean up stack traces by hiding node_modules and the root directory
+
+## Installation
+```bash
+npm install --save trace-cleaner
+```
+
+## Example Usage
+Include in your app:
+
+```javascript
+require('trace-cleaner')
+```
+
+Or add when running node:
+
+```bash
+node -r trace-cleaner
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+const chain = require('stack-chain');
+const chalk = require('chalk');
+const path = require('path');
+
+const appDir = path.dirname(require.main.filename) + '/';
+
+chain.filter.attach(function (error, frames) {
+  error && console.log(chalk.red(error));
+
+  return frames.filter(function (callSite) {
+    const name = callSite && callSite.getFileName();
+    return (name && name.includes(path.sep) && !name.startsWith('internal'));
+  });
+});
+
+chain.format.replace(function (error, frames) {
+  const lines = [];
+
+  lines.push(error.toString());
+
+  for (let i = 0; i < frames.length; i++) {
+    let frame = ('    at ' + frames[i]);
+
+    if (!frame.includes('node_modules')) {
+      frame = frame.split('(');
+      frame[1] = chalk.cyan(frame[1].slice(0, -1));
+      frame = frame.join('(') + ')';
+
+      frame = frame.split(appDir);
+      frame[1] = chalk.yellow(frame[1].slice(0, -1));
+      frame = frame.join(appDir) + ')';
+
+      frame = frame.replace(appDir, chalk.grey(appDir));
+      lines.push(frame);
+    } else {
+      lines.push(chalk.grey(frame));
+    }
+  }
+
+  return lines.join('\n');
+});
